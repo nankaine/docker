03e27436ca6786e3ac77471a36a78a472f11ad12
nankaine <94109329+nankaine@users.noreply.github.com>
2024-06-03 18:19:40 +0800

    Update Check_Adb_Devices_Online.sh

--- Check_Adb_Devices_Online.sh
+++ Check_Adb_Devices_Online.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 nohup node /root/ws-scrcpy/index.js > /dev/null 2>&1 &
+# 检查进程端口是否存在
+sleep 30
+nc -zv 127.0.0.1:8000
+if [ $? == 0 ]; then
+    echo "进程端口8000正常" $(date +%F) $(date +%T)
+ else
+    echo "进程端口8000异常,退出运行" $(date +%F) $(date +%T)
+    sleep 5
+    exit 1
+fi    
 cd /root/ws-scrcpy
 
 # 创建在线文件
 if [ -e "adbonline.log" ]; then
-    echo "adbonline.log文件存在"
+    echo "adbonline.log文件存在" $(date +%F) $(date +%T)
 else
-    echo "adbonline.log文件不存在,新建此文件"
+    echo "adbonline.log文件不存在,新建此文件" $(date +%F) $(date +%T)
     touch adbonline.log
 fi
 
 # 是否自动重连
 if [ "$AUTO_CONNECT" = "true" ]; then
-    echo "开启自动重连模式"
+    echo "开启自动重连模式" $(date +%F) $(date +%T)
     sleep 20
     # 无限循环
     while true
@@ -27,13 +37,13 @@ if [ "$AUTO_CONNECT" = "true" ]; then
             if [ -n "$current_line" ]; then
             	ping -q -c 10 "$current_line"
             	if [ "$?" == 0 ]; then
-            		echo "网络正常,开始链接: " "$current_line"
+            		echo "网络正常,开始链接: " "$current_line" $(date +%F) $(date +%T)
         			adb connect "$current_line":5555
                 else
-                    echo "网络异常,不进行链接~"
+                    echo "网络异常,不进行链接~" $(date +%F) $(date +%T)
         		fi	
     		else
-        		echo "无在线设备，不管他"
+        		echo "无在线设备，不管他" $(date +%F) $(date +%T)
     		fi
     		sleep 10
         done < adbonline.log
@@ -42,7 +52,7 @@ if [ "$AUTO_CONNECT" = "true" ]; then
 else
     while true
     do
-        echo "关闭自动重连模式"
+        echo "关闭自动重连模式" $(date +%F) $(date +%T)
         sleep 3600
     done
 fi
